86769472000ee268a8a387c048f170466b2c4874
MuhammadAdnanHusien <146621758+MuhammadAdnanHusien@users.noreply.github.com>
2024-07-15 16:37:46 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:uas/firebase_option.dart';
+import 'package:uas/view/home.dart';
+import 'package:uas/view/tabBar_login.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          }
+          if (snapshot.hasData) {
+            return HomeScreen(user: snapshot.data!);
+          }
+          return const TabBarLogin();
+        },
+      ),
+    );
+  }
+}
